442852aed88647994cd8be390ffc387df499f13f
Kane-Magri <56299644+Kane-Magri@users.noreply.github.com>
2020-01-31 01:32:26 +0100

    Finished Game
    Added boss fight and end screen, tried to arrange all possible bugs

---
+++ Monku Game/Assets/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        // load the game scene
+        SceneManager.LoadScene("Level 1");
+    }
+}
